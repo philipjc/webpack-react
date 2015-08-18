e2b1a6ba33e8fcadf9dc8b2a05657e2fdfabc480
PjCox <philipcox83@gmail.com>
2015-08-18 21:25:37 +0100

    first commit

---
+++ app/main.js
@@ -0,0 +1,8 @@
+var component = require('./component');
+var app = document.createElement('div');
+
+require('./main.css');
+
+document.body.appendChild(app);
+
+app.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+var merge = require('webpack-merge');
+
+/* Using resolve rather than join() is the same as navigating to a file with cd,
+    And seen as though webpack likes absolute paths, this is good.
+*/
+var TARGET = process.env.npm_lifecycle_event;
+var ROOT_PATH = path.resolve(__dirname);
+
+var common = {
+  entry: path.resolve(ROOT_PATH, 'app/main'),
+  output: {
+    path: path.resolve(ROOT_PATH, 'build'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: path.resolve(ROOT_PATH, 'app')
+      }
+    ]
+  },
+
+  devServer: {
+    port: 4000,
+    colors: true,
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    })
+  ]
+};
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      colors: true,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
